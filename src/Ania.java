7fe4c804fed5967981ca90dc9e5be02bcf763160
Peter Szczeszynski <pszczeszynski@spatial.is>
2020-06-19 14:30:54 -0400

    Stupid

--- src/Ania.java
+++ src/Ania.java
@@ -1,11 +1,9 @@
-package src;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.awt.Robot;
-import java.awt.Toolkit;
 import java.awt.Rectangle;
 import java.net.ServerSocket;
 
@@ -16,38 +14,69 @@ import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 import java.awt.*;
 import java.awt.image.*;
-import java.io.*;
-import javax.imageio.*;
 import javax.swing.*;
-import java.awt.geom.*;
 
 class Ania {
+    private static ServerSocket serverSocket;
+    private static OutputStream outputStream;
+    private static Socket socket;
     public static void main(String[] args) throws Exception{
         while (true) {
-            ServerSocket serverSocket = new ServerSocket(13085);
-            Socket socket = serverSocket.accept();
+            InitializeSocket();
+            BufferedImage screenShot = GetScreenShot();
+            // screenShot = resize(screenShot, screenShot.getHeight()/4, screenShot.getWidth()/4);
+            int packetSize = 60000;
+            SendImage(screenShot, packetSize);
+            CloseSocket();
+        }
+    }
 
-            OutputStream outputStream = socket.getOutputStream();
+
+    private static void SendInt(int x) throws Exception{
+        byte[] bytes = ByteBuffer.allocate(8).putInt(x).array();
+        outputStream.write(bytes);
+        outputStream.flush();
+    }
 
 
-            BufferedImage screenShot = deepCopy(GetScreenShot());
-            screenShot = resize(screenShot, screenShot.getHeight()/2, screenShot.getWidth()/2);
+    /// Breaks the image into manageable chunks to send over the tcpip
+    private static void SendImage(BufferedImage image, int packetSize) throws Exception {
+        // first convert the image into bytes
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", byteArrayOutputStream);
+        byte[] allBytes = byteArrayOutputStream.toByteArray();
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ImageIO.write(screenShot, "jpg", byteArrayOutputStream);
 
-            byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array();
+        // SEND THE IMAGES SPECS
+        int totalSize = byteArrayOutputStream.size();
+        SendInt(totalSize);
+        SendInt(packetSize);
 
-            outputStream.write(size);
 
-            byte[] rawBytes = byteArrayOutputStream.toByteArray();
+        byte[] currentSending = new byte[packetSize];
+        int currentStartingIndex = 0;
+        boolean finished = false;
+        while (!finished) {
+            for (int i = 0; i < packetSize; i ++) {
+                if (currentStartingIndex + i >= allBytes.length) {
+                    finished = true;
+                    break;
+                }
+                currentSending[i] = allBytes[currentStartingIndex + i];
+            }
 
-            outputStream.write(rawBytes);
+            outputStream.write(currentSending);
             outputStream.flush();
             System.out.println("Flushed: " + System.currentTimeMillis());
-            System.out.println("Closing: " + System.currentTimeMillis());
-            serverSocket.close();
+
+            currentStartingIndex += packetSize;
         }
+    }
+
+    private static void InitializeSocket() throws Exception {
+        serverSocket = new ServerSocket(13085);
+        socket = serverSocket.accept();
+        outputStream = socket.getOutputStream();
     }
 
     private static BufferedImage GetScreenShot() throws Exception {
@@ -61,22 +90,12 @@ class Ania {
         return robot.createScreenCapture(screenRect);
     }
 
+    /// Resizes an image to the specified dimensions
     private static BufferedImage resize(BufferedImage img, int height, int width) {
         BufferedImage outputImage = new BufferedImage(width, height, img.getType());
-        // scales the input image to the output image
         Graphics2D g2d = outputImage.createGraphics();
         g2d.drawImage(img, 0, 0, width, height, null);
         g2d.dispose();
-
-
-        // ImageIO.write(outputImage, "jpg", outputImage);
-
-        // Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        // BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        // Graphics2D g2d = resized.createGraphics();
-        // resized.flush();
-        // g2d.drawImage(tmp, 0, 0, null);
-        // g2d.dispose();
         return outputImage;
     }
 
@@ -102,10 +121,15 @@ class Ania {
         });
     }
 
-    static BufferedImage deepCopy(BufferedImage bi) {
+    private static BufferedImage deepCopyImage(BufferedImage bi) {
         ColorModel cm = bi.getColorModel();
         boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
         WritableRaster raster = bi.copyData(null);
         return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+
+    private static void CloseSocket() throws Exception {
+        System.out.println("Closing: " + System.currentTimeMillis());
+        serverSocket.close();
     }
 }
--- src/Mom.java
+++ src/Mom.java
@@ -12,30 +12,62 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Mom {
+    private static Socket socket;
+    private static InputStream inputStream;
     public static void main(String[] args) throws Exception {
+        System.out.println("Running mom...");
+        InitializeSocket(args[0]);
+        System.out.println("Listening...");
 
-        System.out.println(System.getProperty("java.version") + ", args: " + args[0]);
-        Socket socket = new Socket(args[0], 13085);
-        InputStream inputStream = socket.getInputStream();
-
-        System.out.println("Reading: " + System.currentTimeMillis());
-
-        byte[] sizeAr = new byte[4];
-        inputStream.read(sizeAr);
-        int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
-
-        byte[] imageAr = new byte[size];
-        inputStream.read(imageAr);
-
-        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageAr));
-
-        System.out.println("Received " + image.getHeight() + "x" + image.getWidth() + ": " + System.currentTimeMillis());
-        // ImageIO.write(image, "jpg", new File(System.getProperty("user.dir")));
-
-        socket.close();
-
+        BufferedImage image = ReadImageFromSocket();
+        
 
         DisplayImage(image);
+
+        socket.close();
+    }
+
+    public static BufferedImage ReadImageFromSocket() {
+        try {
+            int totalImageSize = ReadIntFromSocket();
+            int imageChunkSize = ReadIntFromSocket();
+            System.out.println("receiving image, total size: " + totalImageSize + 
+                " chunk size: " + imageChunkSize);
+    
+            byte[] receivedBytesStitched = new byte[totalImageSize];
+            byte[] receivedChunk = new byte[imageChunkSize];
+            int currentInsertionPoint = 0;
+            while (currentInsertionPoint + imageChunkSize < totalImageSize) {
+                int numBytesRead = inputStream.read(receivedChunk);
+                System.arraycopy(receivedChunk, 0, receivedBytesStitched, currentInsertionPoint, imageChunkSize);
+                if (numBytesRead < 0) {
+                    break;
+                }
+                System.out.println("received bytes!");
+                currentInsertionPoint += imageChunkSize;
+            }
+
+            System.out.println("finished reading bytes");
+            
+    
+            BufferedImage image = ImageIO.read(new ByteArrayInputStream(receivedBytesStitched));
+            System.out.println("Received " + image.getHeight() + "x" + image.getWidth() + ": " + System.currentTimeMillis());
+            return image;
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString());
+        }
+        return null;
+    }
+
+    public static int ReadIntFromSocket() throws Exception {
+        byte[] bytes = new byte[8];
+        inputStream.read(bytes);
+        return ByteBuffer.wrap(bytes).asIntBuffer().get();
+    }
+
+    public static void InitializeSocket(String ip) throws Exception {
+        socket = new Socket(ip, 13085);
+        inputStream = socket.getInputStream();
     }
 
     public static void DisplayImage(BufferedImage image) throws Exception
